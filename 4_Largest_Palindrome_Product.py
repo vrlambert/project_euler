c673b5106d8d3e35a18e6e9aa84f5ae9fe19af65
Victor Lambert <vrlambert@gmail.com>
2017-11-30 15:40:35 -0500

    Feat: add a faster implementation to 4

--- 4_Largest_Palindrome_Product.py
+++ 4_Largest_Palindrome_Product.py
@@ -1,17 +1,35 @@
 # start with a basic brute force approach
-
+import time
 
 def is_palindrome(string):
     string = str(string)
     return string == string[::-1]
 
-high_res = -1
+def try_one():
+    high_res = -1
+    for i in range(100, 1000):
+        for j in range(100, 1000):
+            if i*j > high_res:
+                if is_palindrome(i*j):
+                    high_res = i*j
 
-for i in range(100, 1000):
-    for j in range(100, 1000):
-        if i*j > high_res:
-            if is_palindrome(i*j):
-                high_res = i*j
-
-print high_res
+    return high_res
 # answer is 906609
+
+# Faster brute force solution, with hints from the PE pdf
+def try_two():
+    high_res = -1
+
+    for i in reversed(range(100, 1000)):
+        for j in reversed(range(i, 1000)):
+            if i*j > high_res:
+                if is_palindrome(i*j):
+                    high_res = i*j
+    return high_res
+
+t1 = time.time()
+print try_one(), time.time()-t1
+
+# This one is three times faster!
+t2 = time.time()
+print try_two(), time.time()-t2
