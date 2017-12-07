b3dcd839293c84c113dcd0cf06e4d57d3fe66fa7
Victor Lambert <vrlambert@gmail.com>
2017-12-06 23:22:47 -0500

    PE 7: improve the is_prime function using PE guide

--- 7_10001st_prime.py
+++ 7_10001st_prime.py
@@ -13,14 +13,26 @@ def is_prime(number):
     """Checks if a number is prime"""
     if number <= 1:
         return False
+    elif number < 4:
+        return True
     elif number % 2 == 0:
         return False
-    for test_num in range(3, int(floor(sqrt(number))) + 1, 2):
-        if number % test_num == 0:
-            return False
+    elif number < 9:
+        return True
+    elif number % 3 == 0:
+        return False
+    else:
+        for test_num in range(5, int(floor(sqrt(number))) + 1, 6):
+            if number % test_num == 0:
+                return False
+            if number % (test_num + 2) == 0:
+                return False
     # return true if we didn't find any divisors
     return True
 
 # this works fairly quickly to get the right answer,
 # but is definitely not optimized
 print list(first_primes(1000000))[10000]
+
+# note this method would work better with a sieve of eratosthnes or counting
+# the primes on the way up
