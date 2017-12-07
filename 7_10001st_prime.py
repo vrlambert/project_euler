04c7a83b1b8509e09ba3803ec98da0cf503f35d0
Victor Lambert <vrlambert@gmail.com>
2017-12-06 23:13:23 -0500

    Solution for 7, 10001st prime added

---
+++ 7_10001st_prime.py
@@ -0,0 +1,26 @@
+# It's prime sieve time!
+from math import sqrt, floor
+
+# the first one is something I wrote without doing research
+def first_primes(limit):
+    """generates primes up to the given limit"""
+    yield 2
+    for test_prime in range(3, limit + 1, 2):
+        if is_prime(test_prime):
+            yield test_prime
+
+def is_prime(number):
+    """Checks if a number is prime"""
+    if number <= 1:
+        return False
+    elif number % 2 == 0:
+        return False
+    for test_num in range(3, int(floor(sqrt(number))) + 1, 2):
+        if number % test_num == 0:
+            return False
+    # return true if we didn't find any divisors
+    return True
+
+# this works fairly quickly to get the right answer,
+# but is definitely not optimized
+print list(first_primes(1000000))[10000]
