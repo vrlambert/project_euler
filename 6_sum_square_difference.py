4286824a1cc7cd0b2ee4ca85cf4040071cc205aa
Victor Lambert <vrlambert@gmail.com>
2017-12-06 22:58:06 -0500

    Add a more efficient solution to PE 6

--- 6_sum_square_difference.py
+++ 6_sum_square_difference.py
@@ -1,3 +1,4 @@
+# initial brute force solution
 def sum_of_square(high):
     return sum(x**2 for x in range(high+1))
 
@@ -9,3 +10,12 @@ def main1(high):
 
 print main1(10)
 print main1(100)
+
+# now with some input from the project euler pdf
+def main2(high):
+    sum1 = high*(high+1) / 2
+    sum_sq = (2*high + 1) * (high + 1) * high / 6
+    return sum1 ** 2 - sum_sq
+
+print main2(10)
+print main2(100)
